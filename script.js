7c06dad83a1302eeb2d311f52362f29cdace47e7
Travis DePuy <tdepuy@xmatters.com>
2020-02-19 12:22:43 -0800

    added better response handling and tweaked the Auth header

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 var token = input['JWT Token'];
 
-// Post Meeting to Zoom
 var apiRequest = http.request({
     'endpoint': 'Zoom',
     'headers': {
         'User-Agent': 'Zoom-Jwt-Request',
-        'Content-type': 'application/json'
-    },
-    'auth': {
-        'bearer': token
+        'Content-type': 'application/json',
+        'Authorization': 'Bearer ' + token
     },
     'path': '/users/' + input['schedule_for'] + '/meetings',
     'method': 'POST'
 });
+
+
 var apiResponse = apiRequest.write(input);
-if (apiResponse.statusCode == 200) {
+if (apiResponse.statusCode >= 200 && apiResponse.statusCode < 300) {
     var resp = JSON.parse(apiResponse.body);
     output['Join URL'] = resp['join_url'];
     output['Start URL'] = resp['start_url'];
